cb2baf404e3a81653abdc3a8f7dd9830f591921e
htmlguy <htmlguy999@gmail.com>
2015-05-25 20:19:52 -0400

    SASS + New Scheme

--- gen-sass.sh
+++ gen-sass.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-sass style.sass > style.css
+LTIME=`stat -c %Z style.sass`
+
+while true
+do
+    ATIME=`stat -c %Z style.sass`
+
+    if [[ "$ATIME" != "$LTIME" ]]
+    then
+        sass style.sass > style.css
+        LTIME=$ATIME
+    fi
+    sleep 1
+done
